0c38cab86d5eca1c5a95c77f428dbdbdd745ef67
sarahlopriore <sarah.lopriore@gmail.com>
2019-02-13 15:22:05 -0500

    small changes

--- src/components/ProfileInfo/ProfileInfo.js
+++ src/components/ProfileInfo/ProfileInfo.js
@@ -5,19 +5,19 @@ const ProfileInfo = () => (
     <div className="profile-div">
         <img className="profile-image" src="./IMG_1732.JPG" alt="Sarah Lopriore" />
         <div className="profile-text">
-            <p>
+            <div>
             I am just beginning my career as a developer, I recently finished school and I have learned a lot. So far I have learned HTML, CSS, JavaScript, JQuery, NodeJS, 
             MySQL, Express, and MongoDB. I have worked in the software world previously as a Quality Assurance Associate at Hyland Software. 
             While I was there I developed skills with SQL and working with databases directly. I also have a lot of experience with debugging and resolving issues. 
             I have also worked with Agile methodologies, including SCRUM. I love learning and I am excited to continue to advance my skills and knowledge in this field.
-            </p>
+            </div>
             <br />
-            <p>
+            <div>
             On a more personal note, I have two amazing dogs, Mikey, a border collie mix and Tia, a pitbull mix. I love to travel, to cook, and to go horseback riding. 
             Animals are one of my passions, and I try to spend some of my free time voluntering with rescue horses. 
             My boyfriend and I love hiking and the mountains, so we do hope to be able to move out west soon. 
             This is one of the many reasons I hope to be able to find a remote job opportunity.
-            </p>
+            </div>
         </div>
     </div>
 )
