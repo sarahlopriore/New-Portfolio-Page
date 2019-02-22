8ed599d2a75476a467e56ab81021127b46d6b6fc
sarahlopriore <sarah.lopriore@gmail.com>
2019-02-22 13:04:00 -0500

    Update images and added links to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,9 +3,9 @@ import './Footer.css';
 
 const Footer = () => (
     <footer>
-        <a  href="#" className="icon-link round linkedin fill"><i className="fa fa-linkedin"></i></a>
-        <a href="#" className="icon-link round github fill"><i className="fa fa-github"></i></a>
-        <a href="#" className="icon-link round envelope fill"><i className="fa fa-envelope"></i></a>
+        <a  href="https://www.linkedin.com/in/sarah-lopriore-5b058b168/" className="icon-link round linkedin fill" target="_blank"><i className="fa fa-linkedin"></i></a>
+        <a href="https://github.com/sarahlopriore" className="icon-link round github fill" target="_blank"><i className="fa fa-github"></i></a>
+        <a href="mailto:sarah.lopriore@gmail.com" className="icon-link round envelope fill" target="_blank"><i className="fa fa-envelope"></i></a>
     </footer>
 )
 
