db8ff1a4acd03981b80ddbb25b5aa2974899491d
sarahlopriore <sarah.lopriore@gmail.com>
2019-03-11 21:11:57 -0400

    Updates

---
+++ precache-manifest.3d0278669bafcb2499c606e530b27048.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9d92bf3815cf5142ba0d",
+    "url": "/static/js/main.9d92bf38.chunk.js"
+  },
+  {
+    "revision": "a852b0ebc3c774f96352",
+    "url": "/static/js/1.a852b0eb.chunk.js"
+  },
+  {
+    "revision": "9d92bf3815cf5142ba0d",
+    "url": "/static/css/main.7d28e25f.chunk.css"
+  },
+  {
+    "revision": "cf7735391a344b0fb041a97316ec488d",
+    "url": "/index.html"
+  }
+];
